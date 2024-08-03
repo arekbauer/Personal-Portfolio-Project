ad551dcd8676f218c063172414f2d2751b82c2d7
Arkadiusz Bauer <arek.bauer12@gmail.com>
2024-08-04 00:08:22 +0100

    Finish Rough Notations Setup

--- portfolio/static/portfolio/js/about-me.js
+++ portfolio/static/portfolio/js/about-me.js
@@ -7,23 +7,39 @@ const colours = {
     lighterBlue: '#CCD6F6',
     lightestBlue: '#E6F1FF',
     clearBlue: '#94A3B8',
-    cream: '#64FFDA'
+    cream: '#64FFDA',
+    bluey: '#ccd6f6',
+    blueClear: '#233554'
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.about-me span');
-    const title = document.querySelector('.left h3');
-    const annotations = [];
+    // Define variables
+    const textAnnotations = [];
 
+    const title = document.querySelector('.highlight');
+    const elements = document.querySelectorAll('.about-me span');
+
+    // Define animations
     elements.forEach(element => {
-        const annotation = annotate(element, { type: 'underline', color: colours.cream });
-        annotations.push(annotation);
+        
+        const annotation = annotate(element, { 
+            type: 'highlight', 
+            color: colours.blueClear,
+            iterations: 1,
+        });
+        textAnnotations.push(annotation);
     });
 
-    const a1 = annotate(title, { type: 'highlight', color: colours.cream });
-    const ag = annotationGroup(annotations);
+    const a1 = annotate(title, { 
+        type: 'underline', 
+        color: colours.lighterBlue, 
+        padding: 0
+    });
+
+    // Combine all annotations into one group
+    const allAnnotations = [a1, ...textAnnotations ];
 
     // Show the annotation group with animation
-    a1.show();
-    ag.show();
+    const ag = annotationGroup(allAnnotations);
+    ag.show(); 
 });
