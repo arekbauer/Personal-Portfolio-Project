140032ded2641f76356d6588e42b8f15b60298cc
Arkadiusz Bauer <arek.bauer12@gmail.com>
2024-08-24 00:57:20 +0100

    Changed and Hid Secret Key

--- Personal_Portfolio/settings.py
+++ Personal_Portfolio/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0%9fd+t6w&f-fnkuqxx)#@*r&#(kf$-0s0g$%sr9#w0+-w3ez'
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Production Settings:
